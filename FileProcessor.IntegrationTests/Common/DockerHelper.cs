0a61c618b90578f5531c52ad26df8a2a29372320
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-06-02 08:32:15 +0100

    Update config

--- FileProcessor.IntegrationTests/Common/DockerHelper.cs
+++ FileProcessor.IntegrationTests/Common/DockerHelper.cs
@@ -21,6 +21,7 @@ namespace FileProcessor.IntegrationTests.Common
     using EstateReporting.Database;
     using EventStore.Client;
     using Microsoft.Data.SqlClient;
+    using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
     using SecurityService.Client;
     using Shared.IntegrationTesting;
     using Shared.Logger;
@@ -369,7 +370,18 @@ namespace FileProcessor.IntegrationTests.Common
             environmentVariables.Add($"AppSettings:EstateManagementApi=http://{estateManamgementContainerName}:{DockerHelper.EstateManagementDockerPort}");
             environmentVariables.Add($"AppSettings:ClientId={clientDetails.clientId}");
             environmentVariables.Add($"AppSettings:ClientSecret={clientDetails.clientSecret}");
-            
+
+            var ciEnvVar = Environment.GetEnvironmentVariable("CI");
+            if ((String.IsNullOrEmpty(ciEnvVar) == false) && String.Compare(ciEnvVar, Boolean.TrueString, StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                // we are running in CI 
+                environmentVariables.Add($"AppSettings:TemporaryFileLocation={"/home/runner/bulkfiles/temporary"}");
+
+                String fileProfileConfig =
+                    "[\r\n      {\r\n        \"Id\": \"B2A59ABF-293D-4A6B-B81B-7007503C3476\",\r\n        \"Name\": \"Safaricom Topup\",\r\n        \"ListeningDirectory\": \"/home/runner/bulkfiles/safaricom\",\r\n        \"RequestType\": \"SafaricomTopupRequest\",\r\n        \"OperatorName\": \"Safaricom\",\r\n        \"LineTerminator\": \"\\n\",\r\n        \"FileFormatHandler\": \"SafaricomFileFormatHandler\"\r\n      },\r\n      {\r\n        \"Id\": \"8806EDBC-3ED6-406B-9E5F-A9078356BE99\",\r\n        \"Name\": \"Voucher Issue\",\r\n        \"ListeningDirectory\": \"/home/runner/bulkfiles/voucher\",\r\n        \"RequestType\": \"VoucherRequest\",\r\n        \"OperatorName\": \"Voucher\",\r\n        \"LineTerminator\": \"\\n\",\r\n        \"FileFormatHandler\": \"VoucherFileFormatHandler\"\r\n      }\r\n    ]";
+                environmentVariables.Add($"AppSettings:FileProfiles={fileProfileConfig}");
+            }
+
             if (additionalEnvironmentVariables != null)
             {
                 environmentVariables.AddRange(additionalEnvironmentVariables);
