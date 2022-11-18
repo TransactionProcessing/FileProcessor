12e583e519b9e3792a2b809c54687efc079299e3
Stuart Ferguson <stuart_ferguson1development@outlook.com>
2022-11-18 18:07:05 +0000

    Upgrade to NET 7

--- FileProcessor.BusinessLogic/Common/ConfigurationReaderConnectionStringRepository.cs
+++ FileProcessor.BusinessLogic/Common/ConfigurationReaderConnectionStringRepository.cs
@@ -10,6 +10,7 @@ namespace FileProcessor.BusinessLogic.Common
     using System.Diagnostics.CodeAnalysis;
     using System.Threading;
     using Microsoft.Data.SqlClient;
+    using MySqlConnector;
     using Shared.General;
     using Shared.Repositories;
 
@@ -18,61 +19,50 @@ namespace FileProcessor.BusinessLogic.Common
     {
         #region Methods
 
-        /// <summary>
-        /// Creates the connection string.
-        /// </summary>
-        /// <param name="externalIdentifier">The external identifier.</param>
-        /// <param name="connectionStringType">Type of the connection string.</param>
-        /// <param name="connectionString">The connection string.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
         public async Task CreateConnectionString(String externalIdentifier,
-                                                 ConnectionStringType connectionStringType,
+                                                 String connectionStringIdentifier,
                                                  String connectionString,
                                                  CancellationToken cancellationToken)
         {
             throw new NotImplementedException("This is only required to complete the interface");
         }
 
-        /// <summary>
-        /// Deletes the connection string configuration.
-        /// </summary>
-        /// <param name="externalIdentifier">The external identifier.</param>
-        /// <param name="connectionStringType">Type of the connection string.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
         public async Task DeleteConnectionStringConfiguration(String externalIdentifier,
-                                                              ConnectionStringType connectionStringType,
+                                                              String connectionStringIdentifier,
                                                               CancellationToken cancellationToken)
         {
             throw new NotImplementedException("This is only required to complete the interface");
         }
 
-        /// <summary>
-        /// Gets the connection string.
-        /// </summary>
-        /// <param name="externalIdentifier">The external identifier.</param>
-        /// <param name="connectionStringType">Type of the connection string.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns></returns>
         public async Task<String> GetConnectionString(String externalIdentifier,
-                                                      ConnectionStringType connectionStringType,
+                                                      String connectionStringIdentifier,
                                                       CancellationToken cancellationToken)
         {
             String connectionString = string.Empty;
             String databaseName = string.Empty;
-            switch (connectionStringType)
-            {
-                case ConnectionStringType.ReadModel:
-                    databaseName = "EstateReportingReadModel" + externalIdentifier;
-                    connectionString = ConfigurationReader.GetConnectionString("EstateReportingReadModel");
-                    break;
-                default:
-                    throw new NotSupportedException($"Connection String type [{connectionStringType}] is not supported");
-            }
 
-            DbConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString)
+            String databaseEngine = ConfigurationReader.GetValue("AppSettings", "DatabaseEngine");
+
+            databaseName = $"{connectionStringIdentifier}{externalIdentifier}";
+            connectionString = ConfigurationReader.GetConnectionString(connectionStringIdentifier);
+
+            DbConnectionStringBuilder builder = null;
+
+            if (databaseEngine == "MySql")
             {
-                InitialCatalog = databaseName
-            };
+                builder = new MySqlConnectionStringBuilder(connectionString)
+                {
+                    Database = databaseName
+                };
+            }
+            else
+            {
+                // Default to SQL Server
+                builder = new SqlConnectionStringBuilder(connectionString)
+                {
+                    InitialCatalog = databaseName
+                };
+            }
 
             return builder.ToString();
         }
