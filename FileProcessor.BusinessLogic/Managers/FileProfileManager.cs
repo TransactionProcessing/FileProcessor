a90fc500f6e36cfc1c3d77b3bda9dae8af3bc743
StuartFerguson <stuart_ferguson1development@outlook.com>
2026-07-11 21:50:38 +0100

    Remove file profile seeding from startup

--- FileProcessor.BusinessLogic/Managers/FileProfileManager.cs
+++ FileProcessor.BusinessLogic/Managers/FileProfileManager.cs
@@ -17,31 +17,16 @@ namespace FileProcessor.BusinessLogic.Managers;
 
 public class FileProfileManager : IFileProfileManager
 {
-    private readonly List<FileProfileModel> SeedProfiles;
-
     private readonly IAggregateRepository<FileProfileAggregateRoot, DomainEvent> FileProfileAggregateRepository;
 
-    public FileProfileManager(List<FileProfileModel> seedProfiles,
-                              IAggregateRepository<FileProfileAggregateRoot, DomainEvent> fileProfileAggregateRepository)
+    public FileProfileManager(IAggregateRepository<FileProfileAggregateRoot, DomainEvent> fileProfileAggregateRepository)
     {
-        this.SeedProfiles = seedProfiles;
         this.FileProfileAggregateRepository = fileProfileAggregateRepository;
-    }
-
-    public async Task<Result> EnsureSeededFileProfiles(CancellationToken cancellationToken)
-    {
-        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken, true);
-        if (aggregateResult.IsFailed)
-        {
-            return ResultHelpers.CreateFailure(aggregateResult);
-        }
-
-        return Result.Success();
     }
 
     public async Task<Result<List<FileProfileModel>>> GetAllFileProfiles(CancellationToken cancellationToken)
     {
-        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken, false);
+        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken);
         if (aggregateResult.IsFailed)
         {
             return ResultHelpers.CreateFailure(aggregateResult);
@@ -52,7 +37,7 @@ public class FileProfileManager : IFileProfileManager
 
     public async Task<Result<FileProfileModel>> GetFileProfile(Guid fileProfileId, CancellationToken cancellationToken)
     {
-        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken, false);
+        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken);
         if (aggregateResult.IsFailed)
         {
             return ResultHelpers.CreateFailure(aggregateResult);
@@ -69,7 +54,7 @@ public class FileProfileManager : IFileProfileManager
 
     public async Task<Result<FileProfileModel>> CreateFileProfile(CreateFileProfileRequest request, CancellationToken cancellationToken)
     {
-        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken, true);
+        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken);
         if (aggregateResult.IsFailed)
         {
             return ResultHelpers.CreateFailure(aggregateResult);
@@ -93,7 +78,7 @@ public class FileProfileManager : IFileProfileManager
 
     public async Task<Result<FileProfileModel>> UpdateFileProfile(Guid fileProfileId, UpdateFileProfileRequest request, CancellationToken cancellationToken)
     {
-        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken, true);
+        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken);
         if (aggregateResult.IsFailed)
         {
             return ResultHelpers.CreateFailure(aggregateResult);
@@ -117,7 +102,7 @@ public class FileProfileManager : IFileProfileManager
 
     public async Task<Result> ArchiveFileProfile(Guid fileProfileId, CancellationToken cancellationToken)
     {
-        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken, true);
+        Result<FileProfileAggregateRoot> aggregateResult = await this.LoadAggregate(cancellationToken);
         if (aggregateResult.IsFailed)
         {
             return ResultHelpers.CreateFailure(aggregateResult);
@@ -132,7 +117,7 @@ public class FileProfileManager : IFileProfileManager
         return await this.FileProfileAggregateRepository.SaveChanges(aggregateResult.Data, cancellationToken);
     }
 
-    private async Task<Result<FileProfileAggregateRoot>> LoadAggregate(CancellationToken cancellationToken, bool seedIfMissing)
+    private async Task<Result<FileProfileAggregateRoot>> LoadAggregate(CancellationToken cancellationToken)
     {
         Result<FileProfileAggregateRoot> aggregateResult = await this.FileProfileAggregateRepository.GetLatestVersion(FileProfileAggregateRoot.FileProfileCollectionId, cancellationToken);
         if (aggregateResult.IsSuccess)
@@ -145,49 +130,6 @@ public class FileProfileManager : IFileProfileManager
             return ResultHelpers.CreateFailure<FileProfileAggregateRoot>(aggregateResult);
         }
 
-        FileProfileAggregateRoot aggregate = FileProfileAggregateRoot.Create();
-        if (seedIfMissing && this.SeedProfiles.Any())
-        {
-            Result<FileProfileAggregateRoot> seeded = this.SeedAggregate(aggregate);
-            if (seeded.IsFailed)
-            {
-                return seeded;
-            }
-
-            Result saveResult = await this.FileProfileAggregateRepository.SaveChanges(seeded.Data, cancellationToken);
-            if (saveResult.IsFailed)
-            {
-                return ResultHelpers.CreateFailure<FileProfileAggregateRoot>(saveResult);
-            }
-
-            return Result.Success(seeded.Data);
-        }
-
-        return Result.Success(aggregate);
-    }
-
-    private Result<FileProfileAggregateRoot> SeedAggregate(FileProfileAggregateRoot aggregate)
-    {
-        foreach (FileProfileModel fileProfile in this.SeedProfiles)
-        {
-            CreateFileProfileRequest request = new CreateFileProfileRequest
-            {
-                FileProfileId = fileProfile.FileProfileId,
-                Name = fileProfile.Name,
-                ListeningDirectory = fileProfile.ListeningDirectory,
-                RequestType = fileProfile.RequestType,
-                OperatorName = fileProfile.OperatorName,
-                LineTerminator = fileProfile.LineTerminator,
-                FileFormatHandler = fileProfile.FileFormatHandler
-            };
-
-            Result createResult = aggregate.CreateProfile(request);
-            if (createResult.IsFailed)
-            {
-                return ResultHelpers.CreateFailure<FileProfileAggregateRoot>(createResult);
-            }
-        }
-
-        return Result.Success(aggregate);
+        return Result.Success(FileProfileAggregateRoot.Create());
     }
 }
--- FileProcessor.BusinessLogic/Managers/IFileProcessorManager.cs
+++ FileProcessor.BusinessLogic/Managers/IFileProcessorManager.cs
@@ -20,8 +20,6 @@ namespace FileProcessor.BusinessLogic.Managers
 
         Task<Result<FileProfileModel>> GetFileProfile(Guid fileProfileId, CancellationToken cancellationToken);
 
-        Task<Result> EnsureSeededFileProfiles(CancellationToken cancellationToken);
-
         Task<Result<List<Models.FileImportLog>>> GetFileImportLogs(Guid estateId, DateTime startDateTime, DateTime endDateTime, Guid? merchantId, CancellationToken cancellationToken);
 
         Task<Result<Models.FileImportLog>> GetFileImportLog(Guid fileImportLogId, Guid estateId, Guid? merchantId, CancellationToken cancellationToken);
